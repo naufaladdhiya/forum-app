abc5dd7828c90c9e0d64e5e5eec459efcf5800d5
naufaladdhiya <naufal.addhiya@gmail.com>
2023-08-16 10:46:17 +0700

    feat: create categoryThread

--- src/pages/threads.pages.jsx
+++ src/pages/threads.pages.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 
 import { getUsersAndThreads } from "../states/shared/action";
@@ -7,6 +7,7 @@ import ThreadList from "../components/threads/threadList.component";
 
 const ThreadsPage = () => {
   const { threads, users, authedUser } = useSelector((states) => states);
+  const [selectedCategory, setSelectedCategory] = useState("");
   const dispatch = useDispatch();
 
   const threadList = threads.map((thread) => ({
@@ -15,16 +16,37 @@ const ThreadsPage = () => {
     authedUser,
   }));
 
+  const filteredThreadList =
+    selectedCategory.length === 0
+      ? threadList
+      : threadList.filter((thread) => thread.category === selectedCategory);
+
+  const handleCategoryChange = (category) => {
+    setSelectedCategory(category === selectedCategory ? "" : category);
+  };
+
   useEffect(() => {
     dispatch(getUsersAndThreads());
   }, [dispatch]);
 
   return (
     <div>
-      <h1 className="text-center text-2xl my-6 font-bold md:text-2xl">
+      <h1 className="text-xl font-bold md:text-2xl mt-12">Pilih Kategori</h1>
+      <div className="flex gap-4">
+        {threads.map((thread, index) => (
+          <div
+            key={index}
+            onClick={() => handleCategoryChange(thread.category)}
+            className="p-2 border-2 border-gray-600 rounded-xl bg-blue-100 opacity-70 text-black font-bold cursor-pointer mt-4 hover:bg-blue-400 hover:opacity-90"
+          >
+            {thread.category}
+          </div>
+        ))}
+      </div>
+      <h1 className="text-center text-2xl my-6 font-bold md:text-3xl mt-10">
         Diskusi Tersedia
       </h1>
-      <ThreadList threads={threadList} />
+      <ThreadList threads={filteredThreadList} />
     </div>
   );
 };
